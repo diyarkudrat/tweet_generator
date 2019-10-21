7f579ea8db8323d4dfe8bcaa301dcfeaa5b8f481
Diyar Kudrat <dkudrat@gmail.com>
2019-10-21 16:32:01 -0700

    added rearrange module

---
+++ src/rearrange.py
@@ -0,0 +1,31 @@
+import random, sys
+
+
+
+def rearrange(arg):
+    args = list(arg)
+
+    for i in range(len(args)):
+
+        l = random.randint(0, i)
+
+        args[i], args[l] = args[l], args[i]
+
+    return args
+
+
+if __name__ == '__main__':
+    input = sys.argv[1:]
+    rearranged = rearrange(input)
+    print(rearranged)
+
+
+
+
+# Example I found from StackOverFlow to assist me with task
+
+# a = [1, 2, 3]
+# a[0], a[2] = a[2], a[0]
+# a
+#
+# [3, 2, 1]
