dd4a0f3e54a733b5233b659b0462687f34707c5d
Diyar Kudrat <dkudrat@gmail.com>
2019-10-22 11:52:45 -0700

    added random sentence module from a text file

---
+++ src/dictionary_words.py
@@ -0,0 +1,34 @@
+import random, sys
+
+# example with own small text file I created
+# def get_words(arg):
+#     with open('words.txt') as file:
+#         # print(list(file))
+#
+#         words = file.read().split()
+#
+#         random_words = []
+#         for i in range(int(arg)):
+#             random_words.append(random.choice(words))
+#
+#         return ' '.join(random_words) + '.'
+
+word_file = '/usr/share/dict/words'
+
+def get_words(arg):
+    with open(word_file, 'r') as file:
+
+        words = file.read().split()
+
+        random_words = []
+        for i in range(int(arg)):
+            random_words.append(random.choice(words))
+
+        return ' '.join(random_words) + '.'
+
+
+
+if __name__ == '__main__':
+    num = sys.argv[1:3]
+    sentence = get_words(num[0])
+    print(sentence)
