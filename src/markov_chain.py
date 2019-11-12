733b9a9ba735355ce10a2f9a227f2347869eed5b
Diyar Kudrat <dkudrat@gmail.com>
2019-11-11 16:36:09 -0800

    generate sentence from markov chain

--- src/markov_chain.py
+++ src/markov_chain.py
@@ -15,7 +15,7 @@ def cleanup_text_file(file_name):
     return words_list
 
 
-def markov_chain(words_list):
+def create_markov_chain(words_list):
 
     markov_dict = dict()
 
@@ -24,16 +24,32 @@ def markov_chain(words_list):
         if markov_dict.get(word) == None:
             markov_dict[word] = Dictogram()
 
-        if index < len(words_list) - 1:
-            next = words_list[index]
+        if index + 1 < len(words_list):
+            next = words_list[index + 1]
             markov_dict.get(word).add_count(next)
 
     return markov_dict
+
+def sentence_gen(markov_chain, length):
+
+    sampled_word = random.choice(list(markov_chain))
+
+    sentence = sampled_word
+
+    for item in range(length - 1):
+        sampled_word = markov_chain[sampled_word].sample()
+
+        sentence += ' ' + sampled_word
+
+    return sentence
 
 
 
 if __name__ == "__main__":
 
     words_list = cleanup_text_file('histo_sample_song.txt')
+    markov_chain = create_markov_chain(words_list)
 
-    print(markov_chain(words_list))
+    print(sentence_gen(markov_chain, 10))
+
+    # print(markov_chain(words_list))
