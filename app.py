7cae51d0cc63c582cb95d0d50e8a6c17eace274c
Diyar Kudrat <diyarkudrat@Diyars-MacBook.local>
2019-12-10 16:35:06 -0800

    trying to figure out txt file issue

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 
-from src.second_order_markov_chain import MarkovChain, cleanup_text_file
+from second_order_markov_chain import MarkovChain, cleanup_text_file
 # from pymongo import MongoClient
 
 # host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/markov')
@@ -22,18 +22,18 @@ def index():
     if request.args.get('sentence length'):
         length = int(request.args.get('sentence_length'))
 
-    if request.args.get('favorite') is not None:
-        favorites.insert_one(
-                {'sentence': markov_chain.sentence,
-                 'likes': 0
-                })
-
-
+    # if request.args.get('favorite') is not None:
+    #     favorites.insert_one(
+    #             {'sentence': markov_chain.sentence,
+    #              'likes': 0
+    #             })
+    #
+    #
     sentence = markov_chain.sentence_gen(length=length)
-    favorites_list = favorites.find()
-    print(favorites_list)
+    # favorites_list = favorites.find()
+    # print(favorites_list)
 
-    return render_template('index.html', sentence=sentence, favorites=favorites_list)
+    return render_template('index.html', sentence=sentence)
 
 
 # if __name__ == '__main__':
