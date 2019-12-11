6b2c5c7cdd17da74387a62ae89b493eeacc77305
Diyar Kudrat <dkudrat@gmail.com>
2019-12-10 16:10:38 -0800

    simplifing app file to get it to deploy

--- app.py
+++ app.py
@@ -37,5 +37,5 @@ def index():
     return render_template('index.html', sentence=sentence, favorites=favorites_list)
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
+# if __name__ == '__main__':
+#     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
